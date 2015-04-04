46c097f746da3c7601d05334415f6cf4636bf209
a2c <a2cgle@gmail.com>
2015-04-04 16:42:37 +0900

    発表時のデモ用に、fetchを一旦外して、直書きｗｗｗｗ

--- client/sw.js
+++ client/sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
-  fetch('localhost:9000/api/things/warning').then(function (data) {
-    var thing = JSON.parse(data);
+  //fetch('http://localhost:9000/api/things/warning').then(function (data) {
+   // var thing = JSON.parse(data);
     var title = '間もなく閉め切り';
-    //var body = 'We have received a push message.';
-    var body = thing.name;
+    var body = 'todo: ここにTodoが入れる';
+    //var body = thing.name;
     var icon = '/images/icon-192x192.png';
     var tag = 'simple-push-demo-notification-tag';
 
@@ -16,5 +16,5 @@ self.addEventListener('push', function(event) {
         tag: tag
       })
     );
-  });
+  //});
 });
